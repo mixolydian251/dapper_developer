162c69c8a6af2544954447935e8fe1d38d012438
mixolydian251 <tjhmusic11@gmail.com>
2018-02-23 20:31:52 -0500

    Added video background

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const Contact = () => (
-  <div className="contact">
-    Here is my contact section
+  <div className="video-container">
+    <div className="video-background">
+      <div className="video-foreground">
+        <iframe src="https://www.youtube.com/embed/AYmZaEVOIIs?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist=AYmZaEVOIIs"
+                frameborder="0"
+                allowfullscreen>
+        </iframe>
+      </div>
+    </div>
+
+    <div className="gradient"> </div>
+
+    <div className="video__banner">
+      <h1 className="video__text">Check out my code on Github!</h1>
+    </div>
+
   </div>
 );
 
